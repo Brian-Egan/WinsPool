b518a810e434c72cf4e24c9bc9d47c716296dcc6
Brian-Egan <bfegan@gmail.com>
2013-09-11 17:43:40 -0400

    Revert 3d35f05..0bbf6c0
    This rolls back to commit 3d35f05a5e66386ea8715fd225a3b9133d497f5c.

--- db/migrate/20130911193801_fix_user_model.rb
+++ db/migrate/20130911193801_fix_user_model.rb
@@ -2,6 +2,6 @@ class FixUserModel < ActiveRecord::Migration
   def change
   	remove_column :users, :name
   	add_column :users, :email, :string
-  	add_column :users, :password_digest, :string
+  	add_column :users, :
   end
 end
