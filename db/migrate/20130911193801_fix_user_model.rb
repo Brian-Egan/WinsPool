a7f2626d65f4d2e120e390a43a4e63107d70854b
Brian-Egan <bfegan@gmail.com>
2013-09-11 22:12:04 -0400

    Added migration for users erhoku

--- db/migrate/20130911193801_fix_user_model.rb
+++ db/migrate/20130911193801_fix_user_model.rb
@@ -1,5 +1,11 @@
 class FixUserModel < ActiveRecord::Migration
+  
+
   def change
+  	unless User.column_names.include? "email"
+  		add_column :users, :email, :string
+  		add_column :users, :password_digest, :string
+  	end
 
   end
 end
